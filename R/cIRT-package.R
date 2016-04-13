f87a1c320e7f69b2055135ee74de067f7992b44c
James J Balamuta <james.balamuta@gmail.com>
2016-04-12 19:50:11 -0500

    Version bump.

--- R/cIRT-package.R
+++ R/cIRT-package.R
@@ -3,8 +3,8 @@
 #' \tabular{ll}{
 #' Package: \tab cIRT\cr
 #' Type: \tab Package\cr
-#' Version: \tab 1.1.0\cr
-#' Date: \tab 2015-12-28\cr
+#' Version: \tab 1.2.0\cr
+#' Date: \tab 2016-04-12\cr
 #' License: \tab GPL-3\cr
 #' }
 #'
