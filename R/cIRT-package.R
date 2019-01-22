bb38987ae353046a9eb3ee04d781e37d8c9cb40b
James Balamuta <balamut2@illinois.edu>
2019-01-21 19:10:22 -0600

    Take a copy of the `DESCRIPTION` file instead of re-writing.
    Fix package aliases

--- R/cIRT-package.R
+++ R/cIRT-package.R
@@ -1,28 +1,4 @@
-#' Item Response Theory Models with Guided Choice
-#'
-#' \tabular{ll}{
-#' Package: \tab cIRT\cr
-#' Type: \tab Package\cr
-#' Version: \tab 1.2.1\cr
-#' Date: \tab 2017-04-25\cr
-#' License: \tab GPL-3\cr
-#' }
-#'
-#' @name cIRT_package
-#' @docType package
-#' @title Item Response Theory Models with Guided Choice
-#' @seealso \code{\link{cIRT}}, \code{\link{TwoPLChoicemcmc}}, 
-#'           \code{\link{probitHLM}}, \code{\link{direct_sum}},
-#'           \code{\link{center_matrix}}, \code{\link{rmvnorm}},
-#'           \code{\link{rwishart}}, \code{\link{riwishart}},
-#'           \code{\link{payout_matrix}}, \code{\link{survey_data}},
-#'           \code{\link{trial_matrix}}, and \code{\link{choice_matrix}}
-#' @author Authors@@R: c(person("Steve", "Culpepper", role = c("aut", "cph"), email =
-#' "sculpepp@illinois.edu"), 
-#' person("James", "Balamuta", role = c("aut","cre", "ctb", "com","cph"),email = "balamut2@illinois.edu"))
-#' 
-#' James Balamuta balamut2@@illinois.edu
-#' @useDynLib "cIRT", .registration=TRUE
+#' @useDynLib cIRT, .registration=TRUE
 #' @importFrom Rcpp evalCpp
-#' @keywords package
-NULL
+#' @aliases cIRT-package
+"_PACKAGE"
